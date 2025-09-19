be895a54f27e621a65970137d27ad1b41531b986
Akhil Biswas <akhil.biswas242006@gmail.com>
2025-09-19 13:23:56 +0530

    Initial commit: Create food ordering database schema
    - Added tables for users
        • students
            ~ departments,
            ~ sections,
            ~ CRs
    - Added menu
        • categories,
        • veg_flag,
        • and item-category
    - Added orders,
        • orders_items,
        • payment,
            ~ payment_type,
            ~ order_status,
            ~ payment_status
    - Added user_fav_items to store user favorite items
    - Defined foreign key constraints and check constraints
    - Ready for future extension with teachers, staff, and additional user types

---
+++ db/schema.sql
@@ -0,0 +1,193 @@
+-- https://drawsql.app/teams/music-express/diagrams/break-time
+-- DDL 
+DROP DATABASE IF EXISTS `break_time`;
+\! echo -e "\e[31m Dtabase deleted\e[0m"
+
+CREATE DATABASE IF NOT EXISTS `break_time`;
+\! echo -e "\e[32m Dtabase Created\e[0m"
+USE `break_time`;
+\! echo -e "\e[33m Dtabase Change\e[0m"
+/*
+DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `student`;
+DROP TABLE IF EXISTS `department`;
+DROP TABLE IF EXISTS `section`;
+DROP TABLE IF EXISTS `cr`;
+DROP TABLE IF EXISTS `menu`;
+\! echo -e "\e[31mALL TABLE deleted\e[0m"
+*/
+
+-- Create users table
+CREATE TABLE IF NOT EXISTS `users`(
+    user_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    f_name char(50) NOT NULL,
+    m_name char(20),
+    l_name char(50) NOT NULL,
+    `Mobile_number` BIGINT,
+    `Email_id` VARCHAR(255),
+    `pass` VARCHAR(255), -- hash password
+    `enrolled_date` DATETIME,
+    `l_modify_date` DATETIME,
+    --
+    CONSTRAINT `chk_mobile_or_email` 
+        CHECK (`Mobile_number` IS NOT NULL OR `Email_id` IS NOT NULL)
+    );
+\! echo -e "\e[32mCreate users table\e[0m"
+
+-- Create student table 
+CREATE TABLE IF NOT EXISTS `student`(
+    `students_id` INT UNSIGNED PRIMARY KEY,
+    `department` INT UNSIGNED,
+    `section` INT UNSIGNED,
+    CONSTRAINT 
+    `user_student` FOREIGN KEY(`students_id`) REFERENCES `users`(`user_id`)
+    );
+\! echo -e "\e[32mCreate user.student table\e[0m"
+-- Create Department table
+CREATE TABLE IF NOT EXISTS `department`(
+    `department_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `department_name` CHAR(255) NOT NULL
+);
+\! echo -e "\e[32mCreate Student Department table\e[0m"
+ALTER TABLE
+    `student` ADD CONSTRAINT
+    `fk_student_department`
+    FOREIGN KEY (`department`) REFERENCES `department`(`department_id`);
+    
+-- Create Section table 
+CREATE TABLE IF NOT EXISTS `section`(
+    `section_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `section` CHAR(255) NOT NULL
+);
+\! echo -e "\e[32mCreate Student Section table\e[0m"
+ALTER TABLE
+    `student` ADD CONSTRAINT
+    `fk_student_section`
+    FOREIGN KEY (`section`) REFERENCES `section`(`section_id`);
+--cr
+CREATE TABLE `cr`(
+    `cr_id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `department_id` INT UNSIGNED NOT NULL,
+    `section_id` INT UNSIGNED NOT NULL,
+    `student_id` INT UNSIGNED NOT NULL,
+    CONSTRAINT `department_cr` FOREIGN KEY(`department_id`) REFERENCES `department`(`department_id`),
+    CONSTRAINT `section_cr` FOREIGN KEY(`section_id`) REFERENCES `section`(`section_id`),
+    CONSTRAINT `student_cr` FOREIGN KEY(`student_id`) REFERENCES `student`(`students_id`)
+);
+\! echo -e "\e[32m cr Table Created\e[0m"
+
+-- menu
+CREATE TABLE `menu`(
+    `item_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `price` INT UNSIGNED NOT NULL,
+    `veg_flag` INT UNSIGNED NOT NULL,
+    `category` INT UNSIGNED,
+    `restaurant` INT UNSIGNED NOT NULL,
+    `enrolled_date` DATETIME,
+    `l_modify_date` DATETIME
+);
+\! echo -e "\e[32m menu Table Created\e[0m"
+
+CREATE TABLE `veg_flag`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `flag_name` VARCHAR(255) NOT NULL UNIQUE,
+    -- Only allow two possible values 
+    CONSTRAINT 
+    `Possible_Value` CHECK(`flag_name` IN ('veg','non_veg'))
+);
+\! echo -e "\e[32m veg_flag Table Created\e[0m"
+ALTER TABLE `menu`
+ADD CONSTRAINT
+`VegType` FOREIGN KEY (`veg_flag`) REFERENCES`veg_flag`(`id`);
+
+CREATE TABLE `category`(
+    `category_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `category` VARCHAR(255) NOT NULL
+);
+\! echo -e "\e[32m category Table Created\e[0m"
+CREATE TABLE `item_category`(
+    `item_id` BIGINT UNSIGNED NOT NULL,
+    `category_id` INT UNSIGNED NOT NULL,
+    CONSTRAINT
+    `fk_item` FOREIGN KEY (`item_id`) REFERENCES `menu`(`item_id`),
+    CONSTRAINT
+    `fk_category` FOREIGN KEY (`category_id`) REFERENCES `category`(`category_id`)
+);
+\! echo -e "\e[32m item_category Table Created\e[0m"
+
+CREATE TABLE `user_fav_items`(
+    `user_id` INT UNSIGNED NOT NULL,
+    `item_id` BIGINT UNSIGNED NOT NULL,
+    CONSTRAINT
+    `fk_user_fav` FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`),
+    CONSTRAINT
+    `fk_fav_item` FOREIGN KEY (`item_id`) REFERENCES `menu`(`item_id`)
+);
+\! echo -e "\e[32m user_fav_items Table Created\e[0m"
+
+-- ORDER
+CREATE TABLE `order_status`(
+    `status_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `status_name` CHAR(255) NOT NULL UNIQUE,
+    CONSTRAINT `Possible_Value`
+    CHECK(`status_name`IN ('delivered','return','successful','not available'))
+);
+\! echo -e "\e[32m status_code Table Created\e[0m"
+
+CREATE TABLE `payment_status`(
+    `status_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `status_name` CHAR(5) NOT NULL UNIQUE,
+    CONSTRAINT `Possible_Value`
+    CHECK(`status_name`IN ('due','paid'))
+);
+
+CREATE TABLE `orders`(
+    `order_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `time` DATETIME NOT NULL,
+    `user_id` INT UNSIGNED NOT NULL,
+    `status_id` INT UNSIGNED NOT NULL,
+    `payment_id` BIGINT UNSIGNED NOT NULL,
+    CONSTRAINT
+    `fk_user` FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`),
+    CONSTRAINT
+    `fk_order_status` FOREIGN KEY(`status_id`) REFERENCES `order_status`(`status_id`)
+);
+\! echo -e "\e[32m Orders Table Created\e[0m"
+
+CREATE TABLE `orders_items`(
+    `order_id` BIGINT UNSIGNED NOT NULL,
+    `item_id` BIGINT UNSIGNED NOT NULL,
+    `quantity` INT UNSIGNED NOT NULL,
+    `status_id` INT UNSIGNED NOT NULL,
+    CONSTRAINT
+    `fk_order_id` FOREIGN KEY(`order_id`) REFERENCES `orders`(`order_id`),
+    CONSTRAINT
+    `fk_item_id` FOREIGN KEY(`item_id`) REFERENCES `menu`(`item_id`),
+    CONSTRAINT
+    `fk_order_item_status` FOREIGN KEY(`status_id`) REFERENCES `order_status`(`status_id`)
+);
+\! echo -e "\e[32m orders_items Table Created\e[0m"
+
+CREATE TABLE `payment`(
+    `payment_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `amount` INT UNSIGNED NOT NULL,
+    `payment_type_id` INT UNSIGNED NOT NULL,
+    `status_id` INT UNSIGNED NOT NULL,
+    CONSTRAINT
+    `fk_payment_status` FOREIGN KEY(`status_id`) REFERENCES `payment_status`(`status_id`)
+);
+ALTER TABLE `orders`
+    ADD CONSTRAINT
+    `fk_payment` FOREIGN KEY(`payment_id`) REFERENCES `payment`(`payment_id`);
+\! echo -e "\e[32m payment Table Created\e[0m"
+
+CREATE TABLE `payment_type`(
+    `payment_type_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `payment_name` CHAR(255) NOT NULL UNIQUE,
+    CONSTRAINT `Possible_Value`
+    CHECK(`payment_name`In('COD','UPI'))
+);
+ALTER TABLE `payment`
+    ADD CONSTRAINT `fk_payment_type` FOREIGN KEY(`payment_type_id`) REFERENCES `payment_type`(`payment_type_id`);
+    \! echo -e "\e[32m payment_type Table Created\e[0m"
